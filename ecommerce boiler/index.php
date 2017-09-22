48b3fd10357d76dc4c12dfec85759e57ce82c9be
Bo Dake <boantdake@gmail.com>
2017-09-22 09:54:18 -0400

    Add files via upload
    this is just a skeleton for a full fledged ecommerce site template functionality to follow

---
+++ ecommerce boiler/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html  lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Dake Designs Boutique</title>
+ 			<!-- tab icon -->
+ 			<link rel="icon" href="">
+			<!-- bootstrap 3.3.7 cdn css 
+			<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+			<!-- bootstrap hosted file css 3.3.7 -->
+			<link rel="stylesheet" href="styles/boostrap.min.css">
+			<!-- font awesome 4.7 -->
+			<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+			<!-- custom stylesheet -->
+			<link rel="stylesheet" type="text/css" href="styles/custom.css">
+		</head>
+
+			<body>
+
+
+
+
+
+			    <!-- Jquery CDN 3.2.1
+	 			<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script> -->
+	 			<!-- jquery hosted file js 3.2.1 -->
+	 			<script src="js/jquery.min.js"></script>
+	 			<!-- bootstrap 3.3.7 cdn js in case of issues 
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
+				<!-- bootstrap hosted file js 3.3.7 -->
+				<script src="js/boostrap.min.js"></script>
+				<!-- javascript custom -->
+				<script src="js/js1.js" type="text/javascript"></script>
+			</body>
+
+</html>
